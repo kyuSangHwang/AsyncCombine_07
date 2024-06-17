779a9d59f59918d3040cb11f4666f2b1b1a3c8b1
KS <kyusang.dev@gmail.com>
2024-06-17 13:18:33 +0900

    Clear

--- SimpleView/SimpleView/ContentView.swift
+++ SimpleView/SimpleView/ContentView.swift
@@ -7,38 +7,9 @@
 
 import SwiftUI
 
-private class PersonViewModel: ObservableObject {
-    @Published var firstName = ""
-    @Published var lastName = ""
-    
-    func save() {
-        print("Save to disk")
-    }
-}
-
 struct ContentView: View {
-    @State var message: String = ""
-    @State var dirty: Bool = false
-    @StateObject private var viewModel = PersonViewModel()
-    
     var body: some View {
-        Form {
-            Section("\(self.dirty ? "*" : "")Input fields") {
-                TextField("First name", text: $viewModel.firstName)
-                    .onChange(of: viewModel.firstName) {
-                        self.dirty = true
-                    }
-                
-                TextField("Last name", text: $viewModel.lastName)
-                    .onChange(of: viewModel.lastName) {
-                        self.dirty = true
-                    }
-            }
-            .onSubmit {
-                viewModel.save()
-                self.dirty = false
-            }
-        }
+        Text("")
     }
 }
 
---
+++ StateManagement/StateManagement/StateManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateManagementApp.swift
+//  StateManagement
+//
+//  Created by 황규상 on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct StateManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
