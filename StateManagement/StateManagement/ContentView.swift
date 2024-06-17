dd9dacf3cf7359bca459898fbe7437b47134e34f
KS <kyusang.dev@gmail.com>
2024-06-17 13:34:42 +0900

    StateObject

--- StateManagement/StateManagement/ContentView.swift
+++ StateManagement/StateManagement/ContentView.swift
@@ -5,13 +5,32 @@
 //  Created by 황규상 on 6/17/24.
 //
 
+
 import SwiftUI
 
-struct ContentView: View {
+class Counter: ObservableObject {
+    @Published var count = 0
+}
+
+struct StateStepper: View {
+    @StateObject var stateCounter = Counter()
+
     var body: some View {
-        VStack {
-            Text("Hello, world!")
+        Section(header: Text("@StateObject")) {
+            Stepper("Counter: \(stateCounter.count)", value: $stateCounter.count)
         }
+    }
+}
+
+struct ContentView: View {
+    @State var color: Color = Color.accentColor
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            StateStepper()
+            ColorPicker("Pick a color", selection: $color)
+        }
+        .foregroundStyle(color)
     }
 }
 
