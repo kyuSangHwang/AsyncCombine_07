23244bf00fed2f47159eef3bb01eb5d8129cd3ec
KS <kyusang.dev@gmail.com>
2024-06-17 11:19:35 +0900

    Initialize Project BookShelf

---
+++ BookShelf/BookShelf/BookShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookShelfApp.swift
+//  BookShelf
+//
+//  Created by 황규상 on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct BookShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BookListView()
+        }
+    }
+}
