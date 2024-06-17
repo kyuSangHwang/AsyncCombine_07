b83b9f1cb5e2933f09e9e06c445a21f6a07496f1
KS <kyusang.dev@gmail.com>
2024-06-17 15:14:03 +0900

    Custom List Rows

--- ListView/ListView/ContentView.swift
+++ ListView/ListView/ContentView.swift
@@ -10,6 +10,19 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         List {
+            VStack(alignment: .leading) {
+                Text("Apples")
+                    .font(.headline)
+                Text("Eat one a day")
+                    .font(.subheadline)
+            }
+            
+            VStack(alignment: .leading) {
+                Text("Banana")
+                    .font(.headline)
+                Text("High in potassium")
+                    .font(.subheadline)
+            }
             
         }
     }
