659af1988bb99812741bdae2cda5e270093b01bc
KS <kyusang.dev@gmail.com>
2024-06-18 16:06:06 +0900

    Initialize Project SignUpForm

---
+++ SignUpForm/SignUpForm/SignUpFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpFormApp.swift
+//  SignUpForm
+//
+//  Created by 황규상 on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SignUpFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
