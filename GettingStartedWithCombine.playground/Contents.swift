20fcb352a6c95fe315149cd913c4c1ecb8915408
KS <kyusang.dev@gmail.com>
2024-06-18 11:45:28 +0900

    Combine Playground

--- GettingStartedWithCombine.playground/Contents.swift
+++ GettingStartedWithCombine.playground/Contents.swift
@@ -1,3 +1,38 @@
-import UIKit
+import Foundation
+import Combine
 
-var greeting = "Hello, playground"
+Just(42)
+    .sink { value in
+        print("Received \(value)")
+    }
+
+["Pepperoni", "Mushrooms", "Onions"].publisher
+    .sink { topping in
+        print("\(topping) is a favorite topping for pizza")
+    }
+
+"Combine".publisher
+    .sink { value in
+        print("Hello, \(value)")
+    }
+
+Just("Combine")
+    .sink { value in
+        print("Hello, \(value)")
+    }
+
+
+123.words.publisher
+    .sink { value in
+        print("Hello, \(value)")
+    }
+
+123.description.publisher
+    .sink { value in
+        print("Hello, \(value)")
+    }
+
+String(123).publisher
+    .sink { value in
+        print("Hello, \(value)")
+    }
