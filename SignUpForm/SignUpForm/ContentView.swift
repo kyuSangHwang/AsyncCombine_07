4e596904bfc6c789cf5497b8f453c0b3bbad2d7d
KS <kyusang.dev@gmail.com>
2024-06-18 16:24:51 +0900

    username map assign

--- SignUpForm/SignUpForm/ContentView.swift
+++ SignUpForm/SignUpForm/ContentView.swift
@@ -12,9 +12,15 @@ class SignUpFormViewModel: ObservableObject {
     @Published var password: String = ""
     @Published var passwordConfirmation: String = ""
     
-    @Published var usernameMessage: String = "이름 적어야지?!"
-    @Published var passwordMessage: String = "비밀번호 적어야지?!"
+    @Published var usernameMessage: String = ""
+    @Published var passwordMessage: String = ""
     @Published var isValid: Bool = false
+    
+    init() {
+        $username.map { $0.count >= 3 }.assign(to: &$isValid)
+        $username.map { $0.count >= 3 ? "" : "Username must be at least three characters!"}
+            .assign(to: &$usernameMessage)
+    }
 }
 
 struct ContentView: View {
@@ -33,9 +39,8 @@ struct ContentView: View {
             }
             /// Password
             Section{
-                TextField("Password", text: $viewModel.password)
-                    .textInputAutocapitalization(.none)
-                    .autocorrectionDisabled()
+                SecureField("Password", text: $viewModel.password)
+                SecureField("Repeat password", text: $viewModel.passwordConfirmation)
             } footer: {
                 Text(viewModel.passwordMessage)
                     .foregroundStyle(Color.red)
