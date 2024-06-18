9fe8529198b445fdf2d0542103fd2680fbfa950f
KS <kyusang.dev@gmail.com>
2024-06-18 17:04:09 +0900

    Form Validation with Combine

--- SignUpForm/SignUpForm/ContentView.swift
+++ SignUpForm/SignUpForm/ContentView.swift
@@ -21,10 +21,45 @@ class SignUpFormViewModel: ObservableObject {
         $username.map { $0.count >= 3 }.eraseToAnyPublisher()
     }()
     
+    private lazy var isPasswordEmptyPublisher: AnyPublisher<Bool, Never> = {
+        $password.map(\.isEmpty).eraseToAnyPublisher()
+    }()
+    
+    private lazy var isPasswordMatchingPublisher: AnyPublisher<Bool, Never> = {
+        Publishers.CombineLatest($password, $passwordConfirmation)
+            .map(==)
+        //  .map { $0 == $1 }
+            .eraseToAnyPublisher()
+    }()
+    
+    private lazy var isPasswordValidPublisher: AnyPublisher<Bool, Never> = {
+        Publishers.CombineLatest(isPasswordEmptyPublisher, isPasswordMatchingPublisher)
+            .map { !$0 && $1 }
+            .eraseToAnyPublisher()
+    }()
+    
+    private lazy var isFormValidPublisher: AnyPublisher<Bool, Never> = {
+        Publishers.CombineLatest(isUsernameLengthValidPublisher, isPasswordValidPublisher)
+            .map { $0 && $1 }
+            .eraseToAnyPublisher()
+    }()
+    
+    
     init() {
-        isUsernameLengthValidPublisher.assign(to: &$isValid)
+        isFormValidPublisher.assign(to: &$isValid)
         isUsernameLengthValidPublisher.map { $0 ? "" : "Username must be at least three characters!"}
             .assign(to: &$usernameMessage)
+        
+        Publishers.CombineLatest(isPasswordEmptyPublisher, isPasswordMatchingPublisher)
+            .map { isPasswordEmpty, isPasswordMatching in
+                if isPasswordEmpty {
+                    return "Password must not be empty"
+                } else if !isPasswordMatching {
+                    return "Passwords do not match"
+                }
+                return ""
+            }
+            .assign(to: &$passwordMessage)
     }
 }
 
