74b2a718f4ff404739cbde75355ce069e7dce5bc
KS <kyusang.dev@gmail.com>
2024-06-20 13:09:05 +0900

    isUsernameAvailablePublisher 추가

--- SignUpForm2/SignUpForm2/SignUpForm2ViewModel.swift
+++ SignUpForm2/SignUpForm2/SignUpForm2ViewModel.swift
@@ -6,10 +6,27 @@
 //
 
 import Foundation
+import Combine
 
 class SignUpForm2ViewModel: ObservableObject {
+    typealias Available = Result<Bool, Error>
+    
     @Published var username: String = ""
     @Published var usernameMessage: String = ""
     @Published var isValid: Bool = false
     @Published var showUpdateDialog: Bool = false
+    
+    private var authenticationService = AuthenticationService()
+    
+    private lazy var isUsernameAvailablePublisher: AnyPublisher<Available, Never> = {
+        $username
+            .debounce(for: 0.5, scheduler: RunLoop.main)
+            .removeDuplicates()
+            .flatMap { username -> AnyPublisher<Available,Never> in
+                self.authenticationService.checkUserNameAvailablePublisher(userName: username)
+                    .asResult()
+            }
+            .share()
+            .eraseToAnyPublisher()
+    }()
 }
