b340bb0afa0478d8577a207ad6a00b74662b5f58
KS <kyusang.dev@gmail.com>
2024-06-20 09:48:59 +0900

    Add Form and viewModel Properties

--- SignUpForm2/SignUpForm2/ContentView.swift
+++ SignUpForm2/SignUpForm2/ContentView.swift
@@ -8,17 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var viewModel = SignUpForm2ViewModel()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Section {
+                TextField("Username", text: $viewModel.username)
+                    .textInputAutocapitalization(.never)
+                    .autocorrectionDisabled()
+            } footer: {
+                Text(viewModel.usernameMessage)
+                    .foregroundStyle(Color.red)
+            }
+            
+            Section {
+                Button("Sign ip") {
+                    print("Signing up as \(viewModel.username)")
+                }
+                .disabled(!viewModel.isValid)
+            }
         }
-        .padding()
+        .alert("Please update", isPresented: $viewModel.showUpdateDialog, actions: {
+            Button("Upgrade") {
+                
+            }
+            Button("Not now", role: .cancel) {}
+        }, message: {
+            Text("It looks like you're using an older version of this app. Please update your app.")
+        })
     }
 }
 
 #Preview {
-    ContentView()
+    NavigationStack {
+        ContentView()
+    }
 }
