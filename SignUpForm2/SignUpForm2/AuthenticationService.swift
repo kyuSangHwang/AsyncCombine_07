778cb0f5c8308bd859bcde05667bc7abf3078c92
KS <kyusang.dev@gmail.com>
2024-06-20 17:52:48 +0900

    BackOff Retry

--- SignUpForm2/SignUpForm2/AuthenticationService.swift
+++ SignUpForm2/SignUpForm2/AuthenticationService.swift
@@ -42,7 +42,7 @@ struct AuthenticationService {
                 return (data, response)
             }
         return dataTaskPublisher
-            .retry(10, withDelay: 3) { error in
+            .retry(10, withBackoff: 3) { error in
                 if case APIError.serverError = error {
                     return true
                 }
