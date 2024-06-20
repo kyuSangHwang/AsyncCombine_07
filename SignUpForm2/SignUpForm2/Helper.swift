ad054d7274fdee3bb2facc7efa1edef5581b4b62
KS <kyusang.dev@gmail.com>
2024-06-20 09:50:21 +0900

    AuthenticationService 시작, Helper Method 추가

--- SignUpForm2/SignUpForm2/Helper.swift
+++ SignUpForm2/SignUpForm2/Helper.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import Combine
+
+struct UserNameAvailableMessage: Codable {
+    var isAvailable: Bool
+    var userName: String
+}
+
+struct APIErrorMessage: Decodable {
+    var error: Bool
+    var reason: String
+}
+
+enum APIError: LocalizedError {
+    case invalidResponse
+}
+
+extension Publisher {
+    func asResult() -> AnyPublisher<Result<Output, Failure>, Never> {
+        self.map(Result.success)
+            .catch { error in
+                Just(.failure(error))
+            }
+            .eraseToAnyPublisher()
+    }
+}
