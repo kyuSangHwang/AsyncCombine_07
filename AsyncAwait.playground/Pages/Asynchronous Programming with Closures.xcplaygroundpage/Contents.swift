465ac7ba50dc5b1f37fa96bc4baaa391e509018f
KS <kyusang.dev@gmail.com>
2024-06-24 10:40:50 +0900

    MakeSandwich completion handler

--- AsyncAwait.playground/Pages/Asynchronous Programming with Closures.xcplaygroundpage/Contents.swift
+++ AsyncAwait.playground/Pages/Asynchronous Programming with Closures.xcplaygroundpage/Contents.swift
@@ -1,4 +1,7 @@
 import Foundation
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
 
 public func customerSays(_ message: String) {
     print("[Customer] \(message)")
@@ -13,48 +16,57 @@ public func sandwichMakerSays(_ message: String, waitFor time: UInt32 = 0) {
 }
 
 func toastBread(_ bread: String,
-                completion: @escaping (String) -> Void)
+                completion: (String) -> Void)
 {
-    DispatchQueue.global().async {
-        sandwichMakerSays("Toasting the bread... Standing by...",
-                          waitFor: 5)
-        completion("Crispy \(bread)")
-    }
+    sandwichMakerSays("Toasting the bread... Standing by...", waitFor: 5)
+    completion("Crispy \(bread)")
 }
 
 func slice(_ ingredients: [String],
-           completion: @escaping ([String]) -> Void)
+           completion: ([String]) -> Void)
 {
-    DispatchQueue.global().async {
-        let result = ingredients.map { ingredient in
-            sandwichMakerSays("Slicing \(ingredient)", waitFor: 1)
-            return "sliced \(ingredient)"
-        }
-        completion(result)
+    let result = ingredients.map { ingredient in
+        sandwichMakerSays("Slicing \(ingredient)", waitFor: 1)
+        return "sliced \(ingredient)"
     }
+    completion(result)
 }
 
-func makeSandwich(bread: String, ingredients: [String], condiments: [String]) -> String {
+func makeSandwich(bread: String, ingredients: [String], condiments: [String],
+                  completion: (String) -> Void) {
     sandwichMakerSays("Preparing your sandwich...")
+    
+    //  toastBread(bread, completion: { toasted in
+    //    print("\(bread) is now \(toasted)")
+    //  })
+    //  print("This code will be executed before the bread is toasted")
+    
+    //  toastBread(bread) { toasted in
+    //    print("\(bread) is now \(toasted)")
+    //  }
     
     toastBread(bread) { toasted in
         slice(ingredients) { sliced in
-            sandwichMakerSays("Spreading \(condiments.joined(separator: ", and "))")
+            sandwichMakerSays("Spreading \(condiments.joined(separator: ", and ")) om \(toasted)")
             sandwichMakerSays("Layering \(sliced.joined(separator: ", "))")
             sandwichMakerSays("Putting lettuce on top")
             sandwichMakerSays("Putting another slice of bread on top")
+            
+            completion("\(ingredients.joined(separator: ", ")), \(condiments.joined(separator: ", ")) on \(toasted)")
         }
     }
-    return "End"
 }
 
-sandwichMakerSays("Hello to Cafe Asynchronous")
+sandwichMakerSays("Hello to Cafe Complete, where we handle your order with care.")
 sandwichMakerSays("Please place your order.")
 
 let clock = ContinuousClock()
 let time = clock.measure {
-    let sandwich = makeSandwich(bread: "Rye", ingredients: ["Cucumbers", "Tomatoes"], condiments: ["Mayo", "Mustard"])
-    customerSays("Hmmm.... this looks like a delicious \(sandwich) sandwich!")
+    makeSandwich(bread: "Rye", ingredients: ["Cucumbers", "Tomatoes"], condiments: ["Mayo", "Mustard"]) { sandwich in
+        customerSays("Hmmm.... this looks like a delicious \(sandwich) sandwich!")
+    }
 }
 
 print("Making this sandwich took \(time)")
+
+print("The end.")
