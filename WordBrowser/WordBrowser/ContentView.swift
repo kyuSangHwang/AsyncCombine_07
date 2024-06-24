02e622a90da2737cbbed71727fde58ba69f2444e
KS <kyusang.dev@gmail.com>
2024-06-24 14:39:43 +0900

    뷰모델 생성자 함수, addFavorite 함수 추가

--- WordBrowser/WordBrowser/ContentView.swift
+++ WordBrowser/WordBrowser/ContentView.swift
@@ -53,6 +53,7 @@ struct ContentView: View {
             NavigationStack {
                 AddWordView { word in
                     print(word)
+                    viewModel.addFavorite(word)
                 }
             }
         }
--- WordBrowser/WordBrowser/LibraryViewModel.swift
+++ WordBrowser/WordBrowser/LibraryViewModel.swift
@@ -6,13 +6,28 @@
 //
 
 import Foundation
+import Combine
 
 class LibraryViewModel: ObservableObject {
-    @Published var searchText: String = ""
-    @Published var randomWord: String = "partially"
-    @Published var tips: [String] = ["Swift", "authentication", "authoriazation"]
+    @Published var searchText = ""
+    @Published var randomWord = "partially"
+    @Published var tips: [String] = ["Swift", "authentication", "authorization"]
     @Published var favorites: [String] = ["stunning", "brilliant", "marvelous"]
     
-    @Published var filteredTips: [String] = [String]()
-    @Published var filteredFavorites: [String] = [String]()
+    @Published var filteredTips = [String]()
+    @Published var filteredFavorites = [String]()
+    
+    init() {
+        Publishers.CombineLatest($searchText, $tips).map { query, items in
+            items.filter { item in query.isEmpty ? true : item.contains(query) }
+        }.assign(to: &$filteredTips)
+        
+        Publishers.CombineLatest($searchText, $favorites).map { query, items in
+            items.filter { item in query.isEmpty ? true : item.contains(query) }
+        }.assign(to: &$filteredFavorites)
+    }
+    
+    func addFavorite(_ word: String) {
+        favorites.append(word)
+    }
 }
