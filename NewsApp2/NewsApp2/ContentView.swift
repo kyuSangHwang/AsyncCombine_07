32090694384e28236b95abc4fd2a4c36164d95f6
KS <kyusang.dev@gmail.com>
2024-06-24 10:01:50 +0900

    Initialize AsyncAwait Playground

---
+++ NewsApp2/NewsApp2/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  NewsApp2
+//
+//  Created by Jungman Bae on 6/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var viewModel = NewsViewModel()
+    
+    var body: some View {
+        NavigationStack {
+            List(viewModel.newsItems) { item in
+                VStack(alignment: .leading) {
+                    Text(item.title)
+                        .font(.headline)
+                    Text(item.pubDate)
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                }
+            }
+            .listStyle(PlainListStyle())
+            .navigationTitle("뉴스 검색 (\(viewModel.newsItems.count) 건)")
+            .searchable(text: $viewModel.searchQuery, prompt: "검색어를 입력하세요")
+            .overlay(
+                Group {
+                    if let errorMessage = viewModel.errorMessage {
+                        Text(errorMessage)
+                            .foregroundColor(.red)
+                            .padding()
+                    }
+                }
+            )
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
