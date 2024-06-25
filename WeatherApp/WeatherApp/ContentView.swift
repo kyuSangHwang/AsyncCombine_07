9db7da61f0f653bc3f6719b07bcee0906101e0bb
KS <kyusang.dev@gmail.com>
2024-06-25 14:12:25 +0900

    Clear

--- WeatherApp/WeatherApp/ContentView.swift
+++ WeatherApp/WeatherApp/ContentView.swift
@@ -9,29 +9,9 @@ import SwiftUI
 import CoreLocation
 
 struct ContentView: View {
-    @StateObject private var viewModel = WeatherViewModel()
-    
     var body: some View {
         VStack {
-            if let weather = viewModel.weather {
-                Text("Temperature: \(weather.temperature)°C")
-                Text("Description: \(weather.description)")
-                Text("Humidity: \(weather.humidity)%")
-                Text("Wind Speed: \(weather.windSpeed) m/s")
-            } else if viewModel.isLoading {
-                ProgressView("Loading...")
-            } else {
-                Text("Enter location to get weather information.")
-            }
-            
-            TextField("Enter location", text: $viewModel.locationText)
-                .padding()
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            
-            Button("Fetch Weather") {
-                viewModel.fetchWeather()
-            }
-            .padding()
+            Text("WeatherApp")
         }
         .padding()
     }
