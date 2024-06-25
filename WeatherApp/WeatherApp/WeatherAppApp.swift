4098dea6cf6aca2a8cf07c4a428e7382571162d1
KS <kyusang.dev@gmail.com>
2024-06-25 11:13:58 +0900

    Initialize WeatherApp Project

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by 황규상 on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
