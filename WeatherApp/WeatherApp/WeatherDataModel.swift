84768dcf978558d8d98093bcf10992dded606342
KS <kyusang.dev@gmail.com>
2024-06-25 13:23:55 +0900

    WeatherDataModel 구조체 이름 변경

--- WeatherApp/WeatherApp/WeatherDataModel.swift
+++ WeatherApp/WeatherApp/WeatherDataModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct WeatherData: Codable {
+struct WeatherDataModel: Codable {
     let temperature: Double // 섭씨 온도
     let description: String // 날씨 설명
     let humidity: Double    // 습도
